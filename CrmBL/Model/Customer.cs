bed4410cbee6f6c0c678322ace7a7621124b3cf6
Andrew Talanov <andrewtalanov@mail.ru>
2023-01-30 19:08:32 +0500

    Создан проект, добавлены основные сущности.

---
+++ CrmBL/Model/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CrmBL.Model
+{
+    internal class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Check> Checks { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
