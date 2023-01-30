51dab410ee9be27b935aac12d18a1d2afac940d3
Andrew Talanov <andrewtalanov@mail.ru>
2023-01-30 19:08:32 +0500

    Добавьте файлы проекта.

---
+++ CrmBL/Model/Check.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrmBL.Model
+{
+    internal class Check
+    {
+        public int CheckId { get; set; }
+        public int CustomerID { get; set; }
+        public virtual Customer Customer { get; set; }
+        public int SellerId { get; set; }
+        public virtual Seller Seller { get; set; }
+        public DateTime Created { get; set; }
+
+        public virtual ICollection<Sell> Sells { get; set; }
+
+        public override string ToString()
+        {
+            return $"#{CheckId} от {Created.ToString("dd.MM.yy hh:mm:ss")}";
+        }
+    }
+}
